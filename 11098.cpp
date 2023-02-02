f58dd993ec85315cc72149a0851bdb304eb82593
NOWEAZ <kimgamja01@gmail.com>
2023-02-02 11:25:10 +0900

    문제들

---
+++ 11098.cpp
@@ -0,0 +1,27 @@
+//11098번
+#include <iostream>
+using namespace std;
+int main() {
+
+	int n, p;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> p;
+		int price;
+		string name;
+
+		int max = 0;
+		string ans;
+
+		for (int j = 0; j < p; j++) {
+			cin >> price >> name;
+			if (price > max) {
+				max = price;
+				ans = name;
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
