1929e0ff541a7087b2e951daa27a5203ddbb11ff
NOWEAZ <kimgamja01@gmail.com>
2023-02-07 17:40:21 +0900

    2742번

---
+++ 2742.cpp
@@ -0,0 +1,15 @@
+//2742번
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    if(N < 0 && N > 100000)
+        return 1;
+    for(int i = 0; i < N; i++)
+        cout << N-i << endl;
+    return 0;
+}
