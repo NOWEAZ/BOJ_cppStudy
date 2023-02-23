386b963143b5896f1e267c07434c58a567947d0c
NOWEAZ <kimgamja01@gmail.com>
2023-02-23 11:22:27 +0900

    2441번

---
+++ 2441.cpp
@@ -0,0 +1,20 @@
+//2441번
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int row;
+    cin >> row;
+    if(row < 0 && row > 100)
+        return 1;
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < i; j++)
+            cout << " ";
+        for(int j = row - i; j > 0; j--)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
