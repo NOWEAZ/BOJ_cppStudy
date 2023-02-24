f4350ba51e957205dad6ea4a513261c50415dcd8
NOWEAZ <kimgamja01@gmail.com>
2023-02-24 17:24:22 +0900

    2609번

--- 2609.cpp
+++ 2609.cpp
@@ -2,18 +2,28 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b;
+    int a, b, i = 1;
     int GCD = 1, LCM = 1;
     cin >> a >> b;
 
-    for(int i = 2; i <= a || i <= b; i++) {
-        if(a / i != 0 && b / i != 0) {
-            GCD *= i;
-            LCM = GCD * (a/i) * (b/i); 
-            i = 2;
-            a = a/i;  b = b/i;
+    if (a != 1 && b != 1){
+        if(i == 1)
+            i++;
+        while(i < a && i < b){
+            if(a % i == 0 && b % i == 0){
+                GCD *= i; 
+                a = a / i; b = b / i;
+                LCM = GCD * a * b;
+            }
+            else if(a % i > 0 || b % i >0) 
+                i++;
         }
+    }
+    else{
+        GCD = 1;
+        LCM = a * b;
     }
     cout << GCD << endl << LCM << endl;
     return 0;
 }
+//서로수인 경우에 어떻게 판별할지
