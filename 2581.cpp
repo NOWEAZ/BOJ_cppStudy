5091699cb6d2c932848cf985ae50cf981b351aee
NOWEAZ <kimgamja01@gmail.com>
2023-03-16 09:41:24 +0900

    2581번

--- 2581.cpp
+++ 2581.cpp
@@ -1,26 +1,24 @@
 //2581번
 #include <iostream>
-#include <vector>
-#include <numeric>
 using namespace std;
 int main(){
-    vector<int> arr;
-    int M, N, sum, count = 0;
+    int M, N, sum = 0, min = -1, count = 0;
     cin >> M >> N;
-    for (int i = M; i < N; i++){
+    for (int i = M; i <= N; i++){
         for(int j = 1; j <= i; j++){
             if(i % j == 0)
                 count ++;
         }
-        if(count == 2)
-            arr.push_back(i);
-        count = 0;
+        if(count == 2){
+            if(min == -1)
+                min = i;
+            sum += i;
+        }
+        count =0;
     }
-    if(arr.size() == 0){
+    if(min == -1)
         cout << -1 << endl;
-        return 0;
-    }
-    sum = accumulate(arr.begin(), arr.end(), 0);
-    cout << sum << endl << arr[0] << endl;
+    else
+        cout << sum << endl << min << endl;
     return 0;
 }
