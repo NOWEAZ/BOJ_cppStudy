0275b81b0e08819c083dff921749c176b80bc311
NOWEAZ <kimgamja01@gmail.com>
2023-04-05 13:32:21 +0900

    2711번

--- 2711.cpp
+++ 2711.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main(){
     int T;  cin >> T;
     int N;  string str;
-    for(int i = 0;i < T; i++){
+    for(int i = 0; i < T; i++){
         cin >> N >> str;
-        
+        for(int i = 0; i < str.length(); i++){
+            if(i != N-1)
+                cout << str[i];
+        }
+    cout << endl;
     }
     return 0;
 }
