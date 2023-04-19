12ac13203d38c3e93fdd625356d8632e66485749
NOWEAZ <kimgamja01@gmail.com>
2023-04-19 10:09:47 +0900

    2750번

---
+++ 2750.cpp
@@ -0,0 +1,25 @@
+//2750번
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N, num, tmp;  cin >> N;
+    vector<int> arr;
+    for(int i = 0; i < N; i++){
+        cin >> num;
+        arr.push_back(num);
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = i + 1; j < N; j++){
+            if(arr[i] > arr[j]){
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;                
+            }
+        }
+    }
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
